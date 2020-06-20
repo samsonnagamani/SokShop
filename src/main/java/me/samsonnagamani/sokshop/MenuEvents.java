fe4cce05b813eff79534830407df36ff7efbff6b
samson.nagamani <samson.nagamani@gmail.com>
2020-06-20 22:05:31 +0100

    Fixed Bug

--- src/main/java/me/samsonnagamani/sokshop/MenuEvents.java
+++ src/main/java/me/samsonnagamani/sokshop/MenuEvents.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.InventoryHolder;
 
@@ -43,39 +42,38 @@ public class MenuEvents implements Listener {
     @EventHandler
     public void onSignMenuClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        Block clickedBlock = event.getClickedBlock();
+        BlockState state = clickedBlock != null ? clickedBlock.getState() : null;
+
+        if (state == null) {
+            return;
+        }
+
 
         // Player has to be in world "world_sokshop" to access shops
-        if (player.getWorld().getName().equalsIgnoreCase("world_sokshop")) {
-            Block clickedBlock = event.getClickedBlock();
-            BlockState state = clickedBlock != null ? clickedBlock.getState() : null;
+        if (player.getWorld().getName().equalsIgnoreCase("world_sokshop") &&
+                state instanceof Sign &&
+                ((Sign) state).getLine(2).equalsIgnoreCase(ChatColor.GREEN + "Buy")) {
 
-            if (state == null) {
-                return;
+            Sign sign = (Sign) state;
+            String line0 = sign.getLine(0);
+            String line2 = sign.getLine(2);
+            String line3 = sign.getLine(3);
+
+            // Initialise Sign
+            // Line 3 is used as a password to create a shop sign
+            if (line0.equalsIgnoreCase("[Building Blocks]") && line2.equalsIgnoreCase("Buy") && line3.equalsIgnoreCase("Init: " + "1234")) {
+                sign.setLine(0, ChatColor.LIGHT_PURPLE + "[Building Blocks]");
+                sign.setLine(2, ChatColor.GREEN + "BUY");
+                // Line 3 is set as empty to remove the password from sign
+                sign.setLine(3, "");
+
+                sign.update();
             }
 
-            if (state instanceof Sign) {
-                Sign sign = (Sign) state;
-                String line0 = sign.getLine(0);
-                String line2 = sign.getLine(2);
-                String line3 = sign.getLine(3);
-
-                // Initialise Sign
-                // Line 3 is used as a password to create a shop sign
-                if (line0.equalsIgnoreCase("[Building Blocks]") && line2.equalsIgnoreCase("Buy") && line3.equalsIgnoreCase("Init: " + "1234")) {
-                    sign.setLine(0, ChatColor.LIGHT_PURPLE + "[Building Blocks]");
-                    sign.setLine(2, ChatColor.GREEN + "BUY");
-                    // Line 3 is set as empty to remove the password from sign
-                    sign.setLine(3, "");
-
-                    sign.update();
-                }
-                if (line0.equalsIgnoreCase(ChatColor.LIGHT_PURPLE + "[Building Blocks]") && line2.equalsIgnoreCase(ChatColor.GREEN + "Buy")) {
-                    new PickAccountBuildingMenu(SokShop.getPlayerMenuUtility(player)).open();
-                }
-                return;
+            if (line0.equalsIgnoreCase(ChatColor.LIGHT_PURPLE + "[Building Blocks]") && line2.equalsIgnoreCase(ChatColor.GREEN + "Buy")) {
+                new PickAccountBuildingMenu(SokShop.getPlayerMenuUtility(player)).open();
             }
-        } else if (player.getWorld().getName().equalsIgnoreCase("world")) {
-            MessageManager.playerInfo(player, "You need to be the SokShop world to use access shop");
         }
     }
 }
